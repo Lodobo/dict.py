224cab35c7cd77084052300ae7a5323d7faa74cb
Lodobo <112024562+Lodobo@users.noreply.github.com>
2022-08-29 07:09:21 +0200

    Update jsonl_to_db.py

--- jsonl_to_db.py
+++ jsonl_to_db.py
@@ -1,100 +1,65 @@
-import jsonlines
-import pandas as pd
-
+import pandas as pd    
 from sqlalchemy import create_engine
-from sqlalchemy.dialects.mysql import JSON, TEXT
+from sqlalchemy.dialects.mysql import JSON, TEXT, CHAR
+from alive_progress import alive_it
 
-# tqdm is for progress bar generation.
-from tqdm import tqdm
+# Enter your username and password to your SQL database. 
+user="username"
+pw="password"
 
-# Establish special datatypes for certain columns 
+# Connect to database
+engine = create_engine(f"mysql+pymysql://{user}:{pw}@localhost/")
 
-dtype = {
+# create a database called 'dictionary'
+engine.execute("CREATE DATABASE dictionary")
+
+# Connect to the dictionary database
+engine = create_engine(f"mysql+pymysql://{user}:{pw}@localhost/dictionary")
+
+# These are the desired datatypes for the SQL tables
+datatype = {
+    'word': TEXT,
+    'pos': TEXT,
+    'senses': JSON,
+    'forms': JSON,
+    'synonyms': JSON,
+    'antonyms':JSON,
+    'hypernyms': JSON,
+    'hyponyms': JSON,
+    'sounds': JSON,
+    'lang': CHAR(10),
+    'lang_code': CHAR(3),
     'head_templates': JSON,
     'etymology_text': TEXT,
     'etymology_templates': JSON,
     'inflection_templates': JSON,
-    'sounds': JSON,
-    'forms': JSON,
-    'senses': JSON,
     'translations': JSON,
-    'synonyms': JSON,
-    'hypernyms': JSON,
-    'hyponyms' : JSON,
-    'antonyms':JSON,
-    'derived' : JSON,
+    'derived': JSON,
     'related': JSON,
     'wikipedia': JSON,
     'categories':JSON
     }
 
-# Connect to the mysql database
-engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
-                       .format(user="user_name",
-                               pw="password123",
-                               db="database_name"))
+# Name of the files that are going to be parsed and sent to the database. 
+items = [
+    'articles',
+    'particles',
+    'abbreviations',
+    'determiners',
+    'conjunctions',
+    'pronouns',
+    'prepositions',
+    'adverbs',
+    'adjectives',
+    'verbs',
+    'nouns']
 
-# Main parser function.
-def parsefile(filename):
-    with jsonlines.open("{}.jsonl".format(filename)) as reader:
-        for pydict in tqdm(reader):
-            # Default fields
-            fields = {'pos':[],'head_templates':[],'etymology_text':[],'etymology_templates':[],'inflection_templates':[],'sounds':[],'forms':[],'word':[],'synonyms':[],'hypernyms':[],'hyponyms':[],'antonyms':[],'derived':[],'related':[],'lang':[],'lang_code':[],'senses':[],'translations':[],'wikipedia':[],'categories':[]}
-            
-            # Convert the values of pydict into strings, for dataframe compatibility reasons
-            keys = pydict.keys()
-            values = pydict.values()
-            values = [str(x) for x in values]
-            newDict = { k:v for (k,v) in zip(keys, values)}
-            
-            # Create and merge dataframes with all fields
-            df1 = pd.DataFrame.from_dict([newDict])
-            df2 = pd.DataFrame(fields)
-            df3 = df1.merge(df2, how='right')
-            
-            # Convert and send dataframe object to sql database
-            df3.to_sql("{}".format(filename), con = engine, if_exists = 'append', index=False, chunksize = 1000, method='multi', dtype=dtype)
+# Main function. Creates a dataframe object from the jsonl files and sends them to the database.
+def export_to_db():
+    for item in alive_it(items):
+        df = pd.read_json(path_or_buf="{}.jsonl".format(item), lines=True)
+        df = df.astype(str) 
+        df.to_sql("{}".format(item), con = engine, if_exists = 'append', index=False, chunksize = 1000, method='multi',dtype=datatype)
+        print("\n")
 
-parsefile('articles')
-parsefile('particles')
-parsefile('abbreviations')
-parsefile('determiners')
-parsefile('conjunctions')
-
-# parsefile('pronouns')
-# parsefile('prepositions')
-# parsefile('adverbs')
-# parsefile('adjectives')
-# parsefile('verbs')
-# parsefile('nouns')
-
-import multiprocessing
-
-if __name__ == "__main__":
-    # creating processes
-    
-    p1 = multiprocessing.Process(target=parsefile, args=('pronouns',))
-    p2 = multiprocessing.Process(target=parsefile, args=('prepositions',))
-    p3 = multiprocessing.Process(target=parsefile, args=('adverbs',))
-
-    p1.start()
-    p2.start()
-    p3.start()
-    p1.join()
-    p2.join()
-    p3.join()
-
-    print("Three jobs done !")
-
-    p1 = multiprocessing.Process(target=parsefile, args=('adjectives',))
-    p2 = multiprocessing.Process(target=parsefile, args=('verbs',))
-    p3 = multiprocessing.Process(target=parsefile, args=('nouns',))
-
-    p1.start()
-    p2.start()
-    p3.start()
-    p1.join()
-    p2.join()
-    p3.join()
-
-print("All done !")
+export_to_db()
