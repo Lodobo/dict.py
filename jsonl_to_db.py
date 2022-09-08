60dd1cb6a9586451a80eb47d0490875657ce4106
Lodobo <112024562+Lodobo@users.noreply.github.com>
2022-09-08 17:46:44 +0200

    Update jsonl_to_db.py

--- jsonl_to_db.py
+++ jsonl_to_db.py
@@ -1,7 +1,7 @@
 import pandas as pd    
 from sqlalchemy import create_engine
 from sqlalchemy.dialects.mysql import JSON, TEXT, CHAR
-from alive_progress import alive_it
+from alive_progress import alive_bar
 
 # Enter your username and password to your SQL database. 
 user="username"
@@ -52,16 +52,23 @@ datatype = {
 
 # Main function. Creates a dataframe object from the jsonl files and sends them to the database.
 def export_to_db(fn):
-        
-        # Read file
-        df = pd.read_json(path_or_buf="{}.jsonl".format(fn), lines=True)
-        
-        # Delete rows if words are too long.
-        indexNames = df[ df['word'].str.len() > 30].index 
-        df.drop(indexNames, inplace=True)
-        
-        # Send to mysql database
-        df.to_sql("{}".format(fn), con = engine, if_exists = 'replace', index=False, chunksize = 1000, method='multi',dtype=datatype)
+    
+    # Count lines for progress bar generation
+    with open(f"{fn}.jsonl", 'r') as fp:
+        num_lines = sum(1 for line in fp)
+                
+    with alive_bar(num_lines, title=f"{fn}", title_length=13, spinner=None) as bar: 
+        chunk = pd.read_json(path_or_buf="{}.jsonl".format(fn), lines=True, chunksize=500)
+        df1 = pd.DataFrame(columns=['word','pos','senses','forms','synonyms','antonyms','hypernyms','hyponyms','meronyms','troponyms','holonyms','sounds','lang','lang_code','head_templates','etymology_text','etymology_templates','inflection_templates','coordinate_terms','form_of','translations','source','hyphenation','proverbs','instances','abbreviations','derived','related','wikipedia','categories'])
+        for df2 in chunk:
+            df = pd.concat([df1, df2])
+            x = df.shape[0]
+
+            indexNames = df[ df['word'].str.len() > 30].index
+            df.drop(indexNames, inplace=True)
+            df.to_sql(f"{fn}", con = engine, if_exists = 'append', index=False, chunksize = 500, dtype=datatype)
+
+            bar(x)
 
 
 # Name of the files that are going to be parsed and sent to the database. 
