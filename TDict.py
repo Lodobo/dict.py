8d53bd7f231d42f2a14de5e23ee063a489b6cee4
Lodobo <112024562+Lodobo@users.noreply.github.com>
2022-09-12 23:27:47 +0000

    Update TDict.py

--- TDict.py
+++ TDict.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-import argparse, json
+import argparse, json, random
 import pandas as pd    
-
 from sqlalchemy import create_engine, text
 from rich.console import Console
 from rich.panel import Panel
@@ -16,13 +15,8 @@ parser.add_argument("-l", "--language",nargs='?',type=str,default="en",help="Sel
 parser.add_argument("-e", "--examples", help="Show examples", action="store_true")
 parser.add_argument("-E", "--etymology", help="Show the etymology of the word", action="store_true")
 parser.add_argument("-s", "--synonyms", help="Show synonyms and antonyms", action="store_true")
-
-
+parser.add_argument("-r", "--random", help="Discover a random word", action="store_true")
 args = parser.parse_args()
-
-if args.word == "None":
-    print("\nError : Expected one argument. \nRun 'Tdict -h' for help\n")
-    quit()
 
 if args.pos != "all" and args.language != "en":
     print("It isn't possible to search in specific parts of speech for languages other than english")
@@ -62,18 +56,26 @@ elif args.language == "sv":
 elif args.language == "fr":
     table="fr"
 
-user="username"
-pw="password123"
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/dictionary"
+                        .format(user="admin",pw="euthymia"))
 
-engine = create_engine(f"mysql+pymysql://{user}:{pw}@localhost/dictionary")
-
-data = f"SELECT * FROM {table} WHERE word='{args.word}'"
+if args.random:
+    txt = text("select count(1) from en;")
+    x = engine.execute(txt).first()
+    row_count = x[0]
+    row_num = random.randint(0, row_count)
+    data = f"SELECT * FROM {table} LIMIT {row_num},1"
+elif args.word == "None":
+    print("\nError : Expected one argument. \nRun 'Tdict -h' for help\n")
+    quit()
+else:
+    data = f"SELECT * FROM {table} WHERE word='{args.word}'"
 
 df = pd.read_sql(data, con=engine)
 df = df.sort_values(by=['pos'])
 
 def printsynonyms():
-    print("    ",end="")
+    print("   ",end="")
     print(i['word'], end="\t")
     if 'tags' in i:
         tags = i['tags']
@@ -83,12 +85,18 @@ def printsynonyms():
     else:
         print()
 
+if args.random:
+    word = df['word'].iloc[0]
+else:
+    word = args.word
+
+console = Console()
 
 if len(df.index) > 0:
-    console = Console()
-    console.print(Panel(Text(f"{args.word}".upper(), justify="center", style="bold white")))
+    console.print(Panel(Text(f"{word}".upper(), justify="center", style="bold white")))
 else:
     print('\n404: Word not found in dictionary\n')
+
 
 for index, row in df.iterrows():
     pos = row['pos']
@@ -96,58 +104,51 @@ for index, row in df.iterrows():
 
     if pos == "INTJ":
         pos = "INTERJECTION"
-
-    if args.etymology == True:
-        if 'etymology_text' in row and row['etymology_text'] != 'null':
-            etymology = Text(row['etymology_text'])
-            etymology = Padding(etymology, (0,4))
-            console.print("\n[bold white][underline]Etymology[/underline] : \n")
-            console.print(etymology)
-            quit()
+    
+    console.print(Panel(f"[bold white] {pos}", width=6 + len(pos)))
 
     if args.synonyms == True:
-        if 'synonyms' in row and row['synonyms'] != 'null':
+        console.print("\n[bold white][underline]Synonyms[/underline] : \n")
+        if row['synonyms'] != 'null':
             tmp = json.loads(row['synonyms'])
-            console.print("\n[bold white][underline]Synonyms[/underline] : \n")
             for i in tmp:
                 printsynonyms()
-        elif 'antonyms' in row and row['antonyms'] != 'null':
+        else:
+            print("   No synonyms in dictionary\n")
+        if row['antonyms'] != 'null':
             tmp = json.loads(row['antonyms'])
             console.print("\n[bold white][underline]Antonyms[/underline] : \n")
             for i in tmp:
                 printsynonyms()
-        elif 'hypernyms' in row and row['hypernyms'] != 'null':
+        if row['hypernyms'] != 'null':
             tmp = json.loads(row['hypernyms'])
             console.print("\n[bold white][underline]Hypernyms[/underline] : \n")
             for i in tmp:
                 printsynonyms()
-        elif 'hyponyms' in row and row['hyponyms'] != 'null':
+        if row['hyponyms'] != 'null':
             tmp = json.loads(row['hyponyms'])
             console.print("\n[bold white][underline]Hyponyms[/underline] : \n")
             for i in tmp:
                 printsynonyms()
-        elif 'meronyms' in row and row['meronyms'] != 'null':
+        if row['meronyms'] != 'null':
             tmp = json.loads(row['meronyms'])
             console.print("\n[bold white][underline]Meronyms[/underline] : \n")
             for i in tmp:
                 printsynonyms()
-        elif 'holonyms' in row and row['holonyms'] != 'null':
+        if row['holonyms'] != 'null':
             tmp = json.loads(row['holonyms'])
             console.print("\n[bold white][underline]holonyms[/underline] : \n")
             for i in tmp:
                 printsynonyms()
-        elif 'troponyms' in row and row['troponyms'] != 'null':
+        if row['troponyms'] != 'null':
             tmp = json.loads(row['troponyms'])
             console.print("\n[bold white][underline]Troponyms[/underline] : \n")
             for i in tmp:
                 printsynonyms()
-        else:
-            print("\n  No synonyms found in dictionary")
         print()
         quit()
 
     if 'sounds' in row and row['sounds'] != 'null':
-        console.print(Panel(f"[bold white] {pos}", width=6 + len(pos)))
 
         tmp = json.loads(row['sounds'])
         for i in tmp:
@@ -156,22 +157,30 @@ for index, row in df.iterrows():
             elif 'ipa' in i and i['ipa'] != "":
                 console.print(f"[bold]IPA[/bold] : [bright_cyan]{i['ipa']}")
 
-        tmp = json.loads(row['senses'])
+    if args.etymology == True or args.random == True:
+        if 'etymology_text' in row and str(row['etymology_text']) != 'None':
+            etymology = Text(row['etymology_text'])
+            etymology = Padding(etymology, (0,4))
+            console.print("\n[bold white][underline]Etymology[/underline] : \n")
+            console.print(etymology)
+            print()
 
-        console.print("\n[bold][underline]Definitions[/underline] : \n")
-        for count, i in enumerate(tmp, start=1):
-            if 'glosses' in i:
-                glosses = i['glosses']
-                glosses = " > ".join(glosses)
-                glosses = Text(f"{count}. {glosses}\n")
-                glosses = Padding(glosses, (0,2))
-                console.print(glosses)
+    tmp = json.loads(row['senses'])
 
-                if args.examples and 'examples' in i:
-                    example = f"Ex: {i['examples'][0]['text']}\n"
-                    example = example
-                    example = Text(example)
-                    example.stylize("grey70")
-                    example = Padding(example, (0,5))
-                    console.print(example)
+    console.print("\n[bold][underline]Definitions[/underline] : \n")
+    for count, i in enumerate(tmp, start=1):
+        if 'glosses' in i:
+            glosses = i['glosses']
+            glosses = " > ".join(glosses)
+            glosses = Text(f"{count}. {glosses}\n")
+            glosses = Padding(glosses, (0,2))
+            console.print(glosses)
+
+            if args.examples and 'examples' in i:
+                example = f"Ex: {i['examples'][0]['text']}\n"
+                example = example
+                example = Text(example)
+                example.stylize("grey70")
+                example = Padding(example, (0,5))
+                console.print(example)
 print()
